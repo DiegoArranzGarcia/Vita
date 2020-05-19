5f6824e065536daab109272d69fa1e73f210513b
Diego Arranz García <diego.arranz@viseo.com>
2020-05-19 21:55:20 +0200

    Init Identity Server

---
+++ Vita.IdentityServer/Config.cs
@@ -0,0 +1,55 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer4Demo
+{
+    public class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api", "Demo API")
+                {
+                    ApiSecrets = { new Secret("secret".Sha256()) }
+                },
+                new ApiResource("policyserver.runtime"),
+                new ApiResource("policyserver.management")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "vita.spa",
+                    ClientName = "SPA (Code + PKCE)",
+
+                    RequireClientSecret = false,
+                    RequireConsent = false,
+
+                    RedirectUris = { "http://localhost:4200/" },
+                    PostLogoutRedirectUris = { },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+                    AllowedScopes = { "openid", "profile", "email", "api" },
+
+                    AllowOfflineAccess = true,
+                    RefreshTokenUsage = TokenUsage.ReUse
+                }
+            };
+        }
+    }
+}
