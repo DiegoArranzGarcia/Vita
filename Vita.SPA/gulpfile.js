1c5a4f60ff31f6820e2a813bc873b84e96112660
Diego Arranz García <diego.arranz.garcia@hotmail.com>
2020-09-03 13:17:52 +0200

    Added log to tokenize gulp task

--- Vita.SPA/gulpfile.js
+++ Vita.SPA/gulpfile.js
@@ -2,8 +2,13 @@ const replace = require('gulp-replace');
 const { src, dest, series, task } = require('gulp');
 
 function tokenize(cb) {
-  src(['dist/assets/app-settings.json'])
-    .pipe(replace(/"(.*?)": "(.*?)"/g, '"$1": "__$1__"'))
+  src(['dist/assets/*.json'])
+    .pipe(
+      replace(/"(.*?)": "(.*?)"/g, function (match, p1, p2) {
+        console.log(`Tokenizing setting: '${p1}'`);
+        return `"${p1}": "__${p1}__"`;
+      })
+    )
     .pipe(dest(['dist/assets']));
 
   cb();
