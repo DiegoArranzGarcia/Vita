ddd6b8e5f97e88734baf8c14447457ba6e8e9e15
Diego Arranz García <diego.arranz.garcia@hotmail.com>
2020-10-03 19:13:17 +0200

    Merge fix

--- Vita.SPA/src/app/goals/goal.module.ts
+++ Vita.SPA/src/app/goals/goal.module.ts
@@ -6,6 +6,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { GoalService } from './goal.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MomentModule } from 'ngx-moment';
+import { ClickOutsideModule } from 'ng-click-outside';
 
 @NgModule({
   imports: [
